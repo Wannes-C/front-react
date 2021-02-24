cadcb9df4eefa61eec51af6ada0cc3e05f376746
wacools <Wannes.Cools@UGent.be>
2021-02-24 20:02:09 +0100

    Startup

---
+++ src/plugins/DamagePlugin/index.js
@@ -0,0 +1,12 @@
+import Plugin from './PluginComponent'
+import Icon from '@material-ui/icons/BrokenImage';
+
+
+
+
+const pluginObject = {
+    plugin: Plugin,
+    icon: Icon,
+}
+
+export default pluginObject
--- src/plugins/pluginDictionary.js
+++ src/plugins/pluginDictionary.js
@@ -1,9 +1,11 @@
 import ProjectPlugin from "./ProjectPlugin";
-import PluginTemplate from "./PluginTemplate"
+import PluginTemplate from "./PluginTemplate";
+import DamagePlugin from "./DamagePlugin"
 
 const pluginDictionary = {
     project: ProjectPlugin,
     template: PluginTemplate,
+    damage: DamagePlugin,
     // jeroen: PluginJeroen
     //   stg: StgPlugin
   };
